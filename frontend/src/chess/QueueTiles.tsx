956156738644efdb8d376059f87b8163571b1111
Matt <mattleeder@posteo.co.uk>
2026-05-13 17:07:42 +0100

    Added splitData.length < 3 guard before accessing indices

--- frontend/src/chess/QueueTiles.tsx
+++ frontend/src/chess/QueueTiles.tsx
@@ -86,6 +86,10 @@ async function tryJoinQueue(queueName: string, matchFoundState: React.RefObject<
 
   eventSource.current.onmessage = (event) => {
     const splitData = event.data.split(",")
+    if (splitData.length < 3) {
+      console.error("SSE: malformed match data", event.data)
+      return
+    }
     const matchRoom = splitData[0]
     const timeFormatInMilliseconds = splitData[1]
     const incrementInMilliseconds = splitData[2]
