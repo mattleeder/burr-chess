09190a9ec2d93b98967899546789fa6d046f2b1d
Matt <mattleeder@posteo.co.uk>
2026-05-14 17:12:42 +0100

    Added temporary reset password page

---
+++ frontend/src/auth/ResetPasswordPage.tsx
@@ -0,0 +1,10 @@
+import './Auth.css';
+
+export function ResetPasswordPage() {
+  return (
+    <div className="loginTile">
+      <h1>Password Reset</h1>
+      <p>Password reset is not yet available. Please contact an administrator if you need to reset your password.</p>
+    </div>
+  )
+}
